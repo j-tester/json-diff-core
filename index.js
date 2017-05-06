6da338ebf27f31af4470f41a65a43cb4d72e138c
Manthan Mallikarjun <nahtnam@gmail.com>
2017-05-05 18:53:25 -0700

    Add diffJSON and diffURLs

--- index.js
+++ index.js
@@ -1,1 +1,17 @@
-console.log('hello world');
+const diffURLS = require('./lib/diffURLs.js');
+const diffJSON = require('./lib/diffJSON.js');
+const writeCSV = require('./lib/writeCSV.js');
+
+module.exports = {
+  diffURLs(leftURL, rightURL, options) {
+    return diffURLS(leftURL, rightURL, options);
+  },
+
+  diffJSON(leftJSON, rightJSON, options) {
+    return diffJSON(leftJSON, rightJSON, options);
+  },
+
+  writeCSV(outputPath, table) {
+    return writeCSV(outputPath, table);
+  },
+};
